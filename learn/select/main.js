61a3b03b0f1b94bf66c968bd7144eb1dc5e3d8b0
gzshi <shgzone.hust@gmail.com>
2013-03-04 15:01:43 +0800

    Site updated at 2013-03-04 07:01:43 UTC

--- learn/select/main.js
+++ learn/select/main.js
@@ -1,28 +1,39 @@
 var inputString = "学生1楼 学生2楼 教工1楼 教工2楼 缘味轩 麻辣烫 炒饭 煲汤 华莱士 不吃啦！";
+var keywords, count;
 var result = "";
 var times = 0;
 var intervalId = -1;
-var mainDiv;
-var menuDiv;
-var resultDiv;
+var mainDiv = "";
+var menuDiv, resultDiv, mainMsg, luckyBtn, menuIpt, returnBtn, showRst;
 
 function rgba(r, g, b, a) {
 	return ("rgba(" + r + "," + g + "," + b + "," + a + ")");
 }
 
-function init () {
+function DOMInit () {
 	mainDiv = document.getElementById("control");
 	menuDiv = document.getElementById("menu");
 	resultDiv = document.getElementById("result");
+	mainMsg = document.getElementById("message");
+	luckyBtn = document.getElementById("luckyButton");
+	menuIpt = document.getElementById('menuInput');
+	returnBtn = document.getElementById("returnMenu");
+	showRst = document.getElementById('resultText');
+}
+
+function init () {
+	if(mainDiv == "") {
+		DOMInit();
+	}
 	menuDiv.style.display = "none";
 	resultDiv.style.display = "none";
 	mainDiv.style.display = "";
-	mainDiv.style.background = rgba(14, 13, 4, 0.58);
 	if(result == "") {
-		document.getElementById("message").innerText = "事，你做！选择，看我的！";
+		mainMsg.innerText = "事，你做！选择，看我的！";
 	}
 	else {
-		document.getElementById("message").innerText = "选择结果：" + result;
+		mainMsg.innerText = "选择结果：" + result;
+		luckyBtn.innerText = "再来一发";
 	}
 	times = 0;
 	adjustSize();
@@ -32,7 +43,8 @@ function init () {
 function modifyMenu () {
 	menuDiv.style.display = "none";
 	mainDiv.style.display = "";
-	inputString = document.getElementById('menuInput').value;
+	inputString = menuIpt.value;
+	luckyBtn.innerText = "试下手气";
 }
 
 function cancelMenu() {
@@ -40,42 +52,75 @@ function cancelMenu() {
 	mainDiv.style.display = "";
 }
 
+function clearMenu() {
+	menuIpt.value = "";
+}
+
 function openMenu () {
 	menuDiv.style.display = "";
 	mainDiv.style.display = "none";
-	document.getElementById('menuInput').value = inputString;
+	menuIpt.value = inputString;
 }
 
 function okFine () {
 	clearInterval(intervalId);
+	if (returnBtn.innerText == "等不及啦！") {
+		result = keywords[Math.floor(Math.random() * count)];
+	}
 	init();
+}
+
+function ignoreSpace(string) {
+	var temp = string.replace('\n', ' ');
+	while(temp != string) {
+		string = temp;
+		temp = temp.replace('\n', ' ');
+	}
+	temp = temp.split(' ');
+	var len = temp.length;
+	var returnString = "";
+	for (var i = 0; i < len; ++i) {
+		returnString += temp[i];
+		if(temp[i+1] == "" || temp[i+1] == undefined)
+			++i;
+		else
+			returnString += " ";
+	}
+	return returnString;
 }
 
 function getRandomSelection () {
 	times = 0;
+	keywords = ignoreSpace(inputString).split(" ");
+	count = keywords.length;
+	for (var i = 0; i < count; ++i) {
+		if(keywords[i].length > 7) {
+			keywords[i] = keywords[i].substr(0, 7);
+		}
+	}
 	clearInterval(intervalId);
 	mainDiv.style.display = "none";
 	resultDiv.style.display = "";
 	intervalId = setInterval(antimate, 100);
-	document.getElementById("returnMenu").innerText = "等不及啦！";
+	returnBtn.innerText = "等不及啦！";
 }
 
 
 // rgba(12, 34, 99, 0.15)
 // rgb(115, 179, 72);
 
+
+
 function antimate () {
-	var keywords = inputString.split(" ");
-	var count = keywords.length;
 	if(times++ > count + 10) {
 		clearInterval(intervalId);
 		result = keywords[Math.floor(Math.random() * count)];
-		document.getElementById("returnMenu").innerText = "就这样吧！";
+		returnBtn.innerText = "就这样吧！";
 	}
 	else {
 		result = keywords[times % count];
 	}
-	document.getElementById('resultText').innerText = result;
+	showRst.innerText = result;
 }
 
 // Adjust window size
